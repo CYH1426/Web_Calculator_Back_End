7b8a3416c978ad905593404f6908356829c3d3f8
CYH1426 <145691860+CYH1426@users.noreply.github.com>
2023-10-20 21:08:40 +0800

    Add files via upload

---
+++ historic_records.sql
@@ -0,0 +1,2 @@
+INSERT INTO `historic_records` (`id`, `equation`, `answer`) VALUES (97, '5+6*2+cos(0.5*π)', '17');
+INSERT INTO `historic_records` (`id`, `equation`, `answer`) VALUES (98, '17+2', '19');
